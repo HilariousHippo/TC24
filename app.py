72ecad4abfc420b84f05c1dbd6a3d5c0e50ec42e
HilariousHippo <168046587+HilariousHippo@users.noreply.github.com>
2024-05-16 10:30:46 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
 import streamlit as st
-import markdown
 
 def render_markdown_file(markdown_file):
     with open(markdown_file, "r", encoding="utf-8") as file:
         markdown_text = file.read()
-        # Render the markdown content
-        return st.markdown(markdown_text, unsafe_allow_html=True)
+        # Render the markdown content using st.markdown
+        st.markdown(markdown_text, unsafe_allow_html=True)
 
 def main():
     st.title("Welcome to your Lab Manual!")
-    # Path to your modified markdown file
+    # Path to your markdown file
     markdown_file = "lab_manual.md"
     render_markdown_file(markdown_file)
 
